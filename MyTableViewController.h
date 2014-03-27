b450799cd27b4ae11d60ca8e9f69765ad8d75d5f
Soumen Bhattacharya <soumenb@Soumens-MacBook-Pro.local>
2014-03-27 18:41:20 -0400

    first commit

---
+++ MyTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Soumen Bhattacharya on 3/27/14.
+//  Copyright (c) 2014 Soumen Bhattacharya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewController : UITableViewController
+
+@end
